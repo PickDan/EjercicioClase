cc73046742af70d295f3964ee4161b289f30b0ad
Daniel Sebastián <danielsebastiany99@gmail.com>
2024-01-03 10:16:23 -0500

    Modificar screen productos

--- screens/ListaProductos.tsx
+++ screens/ListaProductos.tsx
@@ -1,10 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+
+//Use state
+const [productoid, setproductoid] = useState(0)
+const [objeto, setobjeto] = useState('')
+const [precio, setprecio] = useState(0)
 
 export default function ListaProductos() {
   return (
     <View>
       <Text>ListaProductos</Text>
+      <TextInput
+      placeholder='Id del producto: '
+      keyboardType='numeric'
+      onChangeText={(texto)=>(setproductoid(+texto))}
+      />
+      <Text>{productoid}</Text>
+      <Text>    </Text>
+      <TextInput
+      placeholder='Nombre del producto: '
+      onChangeText={(texto)=>(setobjeto(texto))}
+      />
+      <Text>{objeto}</Text>
+      <Text>    </Text>
+      <TextInput
+      placeholder='Precio del producto: '
+      keyboardType='numeric'
+      onChangeText={(texto)=>(setprecio(+texto))}
+      />
+      <Text>{precio}</Text>
     </View>
   )
 }
